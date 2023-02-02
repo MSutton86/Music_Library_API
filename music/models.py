ddf7312d2e4d63b306ddc9c04816124b9c7ec74d
Mark S <stcoronawraps@gmail.com>
2023-02-02 10:50:08 -0700

    Created music app model + Registered music and restframework in Project Setting Installed apps

--- music/models.py
+++ music/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=85)
+    artist = models.CharField(max_length=85)
+    album = models.CharField(max_length=165)
+    release_date = models.DateField(null=True)
+    genre = models.CharField(max_length=85)
+    
